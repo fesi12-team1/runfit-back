e3a48fadca760c14a783b730833d9c012445ee05
dnjsals45 <dnjsals45@naver.com>
2025-11-29 13:41:41 +0900

    Initial Commit

---
+++ src/main/java/com/runfit/RunfitApplication.java
@@ -0,0 +1,13 @@
+package com.runfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunfitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/runfit/RunfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
